71bfadd84cfdddf7e1b246c704fca3cb1dd1b3ec
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-09-15 16:19:34 -0500

    Make these tests run
    https://github.com/hasii2011/PyUt/issues/1

--- src/tests/TestPyutPreferences.py
+++ src/tests/TestPyutPreferences.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
 
-__version__ = "$Revision: 1.1.1.1 $"
-__author__ = "EI6, eivd, Group Dutoit - Roux"
-__date__ = "2002-05-22"
+import os
 
-import sys
-if ".." not in sys.path:
-    sys.path.append("..") # access to the classes to test
 import unittest
 
 from PyutPreferences import PyutPreferences, PREFS_FILENAME
-import os
+
 
 class TestPyutPreferences(unittest.TestCase):
     """
@@ -28,11 +22,9 @@ class TestPyutPreferences(unittest.TestCase):
         self.prefs.init()
         # test that it's empty
         try:
-            self.failUnless(self.prefs[self.items.keys()[0]] is None)
-        except:
-            self.fail("Should not raise an exception")
-
-    #>------------------------------------------------------------------------
+            self.assertTrue(self.prefs[list(self.items.keys())[0]] is None)
+        except (ValueError, Exception) as e:
+            self.fail(f"Should not raise an exception;  {e}")
 
     def setUp(self):
         """
@@ -48,23 +40,18 @@ class TestPyutPreferences(unittest.TestCase):
             "test_list" : ["comment", "allez", "vous"],
         }
         self.prefs = PyutPreferences()
-        self.emptyPrefs() # empty the prefs
+        self.emptyPrefs()  # empty the prefs
         # fill the prefs
-        for item, value in self.items.items():
+        for item, value in list(self.items.items()):
             self.prefs[item] = value
-
-    #>------------------------------------------------------------------------
 
     def testValues(self):
         """
         Test that the prefs contain the good values.
         """
-        for item, value in self.items.items():
+        for item, value in list(self.items.items()):
             found = self.prefs[item]
-            self.failUnless(found == str(value),
-                "Wrong value for %s. Want %s, got %s" % (item, value, found))
-
-    #>------------------------------------------------------------------------
+            self.assertTrue(found == str(value), "Wrong value for %s. Want %s, got %s" % (item, value, found))
 
     def testSpaceInName(self):
         """
@@ -73,11 +60,9 @@ class TestPyutPreferences(unittest.TestCase):
         try:
             self.prefs["salut les amis"] = 3
         except TypeError:
-            pass # that's OK
+            pass  # that's OK
         else:
             self.fail("A name without spaces has not raised an exception")
-
-    #>------------------------------------------------------------------------
 
     def testLoadSave(self):
         """
@@ -86,22 +71,17 @@ class TestPyutPreferences(unittest.TestCase):
         # now, prefs is already loaded, add some values that are automatically
         # saved
         self.prefs["new_pref"] = 10
-        self.prefs.init() # reinit the object (that's the only way since it's
-                          # a singleton
-        self.failUnless(self.prefs["new_pref"] == "10", "Value doesn't exist")
+        self.prefs.init()  # reinit the object (that's the only way since it's a singleton
+        self.assertTrue(self.prefs["new_pref"] == "10", "Value doesn't exist")
 
-    #>------------------------------------------------------------------------
-
-    def testGetUnknowPref(self):
+    def testGetUnknownPref(self):
         """
         What happens with an unknown pref.
         """
         try:
-            self.failUnless(self.prefs["unknown"] is None)
-        except:
-            self.fail("Should not raise an exception")
-
-    #>------------------------------------------------------------------------
+            self.assertTrue(self.prefs["unknown"] is None)
+        except (ValueError, Exception) as e:
+            self.fail(f"Should not raise an exception {e}")
 
     def testGetUnknownPrefWhenEmpty(self):
         """
@@ -111,8 +91,6 @@ class TestPyutPreferences(unittest.TestCase):
         """
         self.emptyPrefs()
 
-    #>------------------------------------------------------------------------
-
     def testLastOpenedFiles(self):
         """
         Test the last opened files management.
@@ -121,25 +99,16 @@ class TestPyutPreferences(unittest.TestCase):
             "un", "deux", "trois", "quatre", "cinq", "six"
         ]
         self.prefs.setNbLOF(len(files) - 1)
-        self.prefs.init() # reload prefs
-        self.failUnless(self.prefs.getNbLOF() == len(files) - 1, "wrong nbLOF")
+        self.prefs.init()  # reload prefs
+        self.assertTrue(self.prefs.getNbLOF() == len(files) - 1, "wrong nbLOF")
         for file in files:
             self.prefs.addNewLastOpenedFilesEntry(file)
         self.prefs.init()
-        files.reverse() # because it's a last in first out
-        files.pop() # remove last one which should have been dropped
+        files.reverse()  # because it's a last in first out
+        files.pop()      # remove last one which should have been dropped
         for i in range(len(files) - 1):
-            self.failUnless(self.prefs.getLastOpenedFilesList()[i] == files[i],
-                "wrong file name")
-
-    #>------------------------------------------------------------------------
+            self.assertTrue(self.prefs.getLastOpenedFilesList()[i] == files[i], "wrong file name")
 
 
-def suite():
-    """You need to change the name of the test class here also."""
-    return unittest.makeSuite(TestPyutPreferences)
-
-def main():
-    unittest.TextTestRunner().run(suite())
-
-if __name__ == "__main__": main()
+if __name__ == '__main__':
+    unittest.main()
