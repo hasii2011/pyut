5162879fb9530f0a2c9a19463f2355c1ee683bf2
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-09-19 07:45:21 -0500

    Allows IoPython to compile
    https://github.com/hasii2011/PyUt/issues/1

--- src/plugins/ast.py
+++ src/plugins/ast.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-import compiler
 
-#ast = compiler.parseFile("testmulti.py")
-#print ast
+# import compiler # TODO need a replacement for this
 
-class Visitor(compiler.visitor.ASTVisitor):
+from ast import NodeVisitor
+from ast import PyCF_ONLY_AST
+from ast import parse
+from ast import walk
+
+
+# class Visitor(compiler.visitor.ASTVisitor):
+
+
+class Visitor(NodeVisitor):
 
     def __init__(self, className):
+
         self._className = className
-        compiler.visitor.ASTVisitor.__init__(self)
+        # compiler.visitor.ASTVisitor.__init__(self)
+        super().__init__()
         self._result = {}
 
     def getResult(self):
@@ -20,10 +27,10 @@ class Visitor(compiler.visitor.ASTVisitor):
             self.visit(node.code)
 
     def visitAssTuple(self, node):
-        list = []
+        retList = []
         for child in node.nodes:
-            list.append(self.visit(child))
-        return list
+            retList.append(self.visit(child))
+        return retList
 
     def visitAssAttr(self, node):
         return self.visit(node.expr) + "." + node.attrname
@@ -34,9 +41,9 @@ class Visitor(compiler.visitor.ASTVisitor):
             targets.append(self.visit(n))
         expr = self.visit(node.expr)
         if expr is None:
-            print node.expr
+            print(node.expr)
         target = targets[-1]
-        if isinstance(expr, list): # expr is a list
+        if isinstance(expr, list):  # expr is a list
             for i, j in zip(target, expr):
                 if isinstance(i, type("")) and i[0:5] == "self.":
                     if j is not None and isinstance(j, type("")):
@@ -45,7 +52,7 @@ class Visitor(compiler.visitor.ASTVisitor):
                     else:
                         if i[5:] not in self._result:
                             self._result[i[5:]] = ""
-        else: # expr is not a list
+        else:  # expr is not a list
             if isinstance(target, type("")) and target[0:5] == "self.":
                 if expr is not None:
                     if target[5:] not in self._result:
@@ -68,46 +75,52 @@ class Visitor(compiler.visitor.ASTVisitor):
         return node.name
 
     def visitTuple(self, node):
-        list = []
+        retList = []
         for n in node.nodes:
-            list.append(self.visit(n))
-        return list
+            retList.append(self.visit(n))
+        return retList
 
     def visitGetattr(self, node):
         return str(self.visit(node.expr)) + "." + str(node.attrname)
 
-    #def visitAdd(self, node):
+    # def visitAdd(self, node):
     #    return self.visit(node.left) + "+" + self.visit(node.right)
 
-    #def visitFunction(self, node):
+    # def visitFunction(self, node):
     #    print "fonction :", node.name
 
-    #def visitCallFunc(self, node):
+    # def visitCallFunc(self, node):
     #    s = self.visit(node.node) + "("
     #    for n in node.args:
     #        s += self.visit(n) + ", "
     #    s = s[:-2] + ")"
     #    return s
 
-#compiler.walk(ast, Visitor())
+# compiler.walk(ast, Visitor())
+
 
 class FieldExtractor(object):
     def __init__(self, filename):
         self._filename = filename
 
     def getFields(self, className):
+
         visitor = Visitor(className)
-        compiler.walk(compiler.parseFile(self._filename), visitor)
+        # compiler.walk(compiler.parseFile(self._filename), visitor)
+        walk(parse(self._filename, PyCF_ONLY_AST))
+
         return visitor.getResult()
 
-def main():
-    import sys
-    if len(sys.argv) > 1:
-        print compiler.parseFile(sys.argv[1])
-        res = FieldExtractor(sys.argv[1]).getFields(sys.argv[2])
-        print "-" * 76
-        for name, val in res.items():
-            print name, "=", val
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     import sys
+#     if len(sys.argv) > 1:
+#         print(compiler.parseFile(sys.argv[1]))
+#         res = FieldExtractor(sys.argv[1]).getFields(sys.argv[2])
+#         print("-" * 76)
+#         for name, val in list(res.items()):
+#             print(name, "=", val)
+#
+#
+# if __name__ == "__main__":
+#     main()
