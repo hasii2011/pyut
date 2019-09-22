b4dcbdb824b5798cc743c436de1ad1b6303a6761
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-09-22 13:48:48 -0500

    Help Dialog comes up
    https://github.com/hasii2011/PyUt/issues/1

--- src/DlgHelp.py
+++ src/DlgHelp.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-__version__ = "$Revision: 1.7 $"
-__author__ = "EI5, eivd, Group Burgbacher - Waelti"
-__date__ = "2001-11-14"
-# C.Dutoit, 20020917, removed OnLoadFile / load file button... (why was it here?)
 
-#from wxPython.wx import *
-#from wxPython.html import *
-import wx
+from os import getcwd
+from os import path as osPath
+from os import sep as osSeparator
+
+from wx import ALL
+from wx import BOTH
+from wx import BOTTOM
+from wx import EVT_BUTTON
+from wx import GROW
+from wx import HORIZONTAL
+from wx import ID_OK
+from wx import VERTICAL
+
+from wx import Dialog
+from wx import MessageBox
+from wx import Size
+from wx import BoxSizer
+from wx import Button
+from wx import DefaultPosition
+
+from wx.html import HtmlWindow
+from wx.html import HtmlEasyPrinting
+
+from wx.lib.dialogs import ScrolledMessageDialog
+
 from pyutUtils import assignID
+
+from globals import _
 
 [ID_LOAD_FILE, ID_BACK, ID_FORWARD, ID_PRINT, ID_VIEW_SOURCE] = assignID(5)
 
-class DlgHelp(wx.Dialog):
+
+class DlgHelp(Dialog):
     """
     Pyut help dialog frame. Used to show help and navigate through it.
 
@@ -25,9 +44,6 @@ class DlgHelp(wx.Dialog):
     :author: C.Dutoit
     :contact: dutoitc@hotmail.com
     """
-
-    #>------------------------------------------------------------------------
-
     def __init__(self, parent, ID, title):
         """
         Constructor.
@@ -35,50 +51,43 @@ class DlgHelp(wx.Dialog):
         @since 1.0
         @author C.Dutoit
         """
-        import os, wx
-        #dialog box
-        wx.Dialog.__init__(self, parent, ID, title, wx.DefaultPosition, 
-                          wx.Size(720, 520))
-        self.Center(wx.BOTH)
+        # dialog box
+        super().__init__(parent, ID, title, DefaultPosition, Size(720, 520))
 
-        #window
-        import wx.html
-        self.html = wx.html.HtmlWindow(self, -1, wx.DefaultPosition, wx.Size(720, 520))
-        self.html.LoadPage(os.getcwd()+ os.sep + "help" + os.sep + "index.html")
+        self.Center(BOTH)
 
-        # Printer
-        self.printer=wx.html.HtmlEasyPrinting()
+        self.html = HtmlWindow(self, -1, DefaultPosition, Size(720, 520))
+        #
+        # TODO Change this to load as a resource
+        #
+        self.html.LoadPage(getcwd() + osSeparator + "help" + osSeparator + "index.html")
 
-        # Sizer
-        self.box = wx.BoxSizer(wx.VERTICAL)
-        self.box.Add(self.html, 1, wx.GROW)
-        subbox = wx.BoxSizer(wx.HORIZONTAL)
+        self.printer = HtmlEasyPrinting()
 
-        #buttons
-        #btn = wx.Button(self, ID_LOAD_FILE, _("Load file"))
-        #EVT_BUTTON(self, ID_LOAD_FILE, self.__OnLoadFile)
-        #subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
+        self.box = BoxSizer(VERTICAL)
+        self.box.Add(self.html, 1, GROW)
+        subbox = BoxSizer(HORIZONTAL)
 
-        btn = wx.Button(self, ID_BACK, _("Back"))
-        self.Bind(wx.EVT_BUTTON, self.__OnBack, id=ID_BACK)
-        subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
-        
-        btn = wx.Button(self, ID_FORWARD, _("Forward"))
-        self.Bind(wx.EVT_BUTTON, self.__OnForward, id=ID_FORWARD)
-        subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
+        btn = Button(self, ID_BACK, _("Back"))
+        self.Bind(EVT_BUTTON, self.__OnBack, id=ID_BACK)
+        subbox.Add(btn, 1, GROW | ALL, 2)
 
-        btn = wx.Button(self, ID_PRINT, _("Print"))
-        self.Bind(wx.EVT_BUTTON, self.__OnPrint, id=ID_PRINT)
-        subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
+        btn = Button(self, ID_FORWARD, _("Forward"))
+        self.Bind(EVT_BUTTON, self.__OnForward, id=ID_FORWARD)
+        subbox.Add(btn, 1, GROW | ALL, 2)
 
-        btn = wx.Button(self, ID_VIEW_SOURCE, _("View Source"))
-        self.Bind(wx.EVT_BUTTON, self.__OnViewSource, id=ID_VIEW_SOURCE)
-        subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
+        btn = Button(self, ID_PRINT, _("Print"))
+        self.Bind(EVT_BUTTON, self.__OnPrint, id=ID_PRINT)
+        subbox.Add(btn, 1, GROW | ALL, 2)
 
-        btn = wx.Button(self, wx.ID_OK, _("Exit"))
-        subbox.Add(btn, 1, wx.GROW | wx.ALL, 2)
+        btn = Button(self, ID_VIEW_SOURCE, _("View Source"))
+        self.Bind(EVT_BUTTON, self.__OnViewSource, id=ID_VIEW_SOURCE)
+        subbox.Add(btn, 1, GROW | ALL, 2)
 
-        self.box.Add(subbox, 0, wx.GROW | wx.BOTTOM)
+        btn = Button(self, ID_OK, _("Exit"))
+        subbox.Add(btn, 1, GROW | ALL, 2)
+
+        self.box.Add(subbox, 0, GROW | BOTTOM)
         self.SetSizer(self.box)
         self.SetAutoLayout(True)
         subbox.Fit(self)
@@ -88,10 +97,6 @@ class DlgHelp(wx.Dialog):
 
         self.Show(True)
 
-
-
-    #>------------------------------------------------------------------------
-
     def OnShowDefault(self, event):
         """
         Show default page
@@ -99,27 +104,21 @@ class DlgHelp(wx.Dialog):
         @since 1.1
         @author C.Dutoit
         """
-        import os
-        name = os.path.join(os.getcwd(), 'help/index.html')
+        name = osPath.join(getcwd(), 'help/index.html')
         self.html.LoadPage(name)
 
-    #>------------------------------------------------------------------------
-
-    #def __OnLoadFile(self, event):
-        #"""
-        #Load a html page
+    # def __OnLoadFile(self, event):
+        # """
+        # Load a html page
 #
-        #@since 1.1
-        #@author C.Dutoit
-        #"""
-        #dlg = wx.FileDialog(self, wildcard = '*.htm*', style=wx.OPEN)
-        #if dlg.ShowModal()==wx.OK:
-            #path = dlg.GetPath()
-            #self.html.LoadPage(path)
-        #dlg.Destroy()
-
-
-    #>------------------------------------------------------------------------
+        # @since 1.1
+        # @author C.Dutoit
+        # """
+        # dlg = wx.FileDialog(self, wildcard = '*.htm*', style=wx.OPEN)
+        # if dlg.ShowModal()==wx.OK:
+            # path = dlg.GetPath()
+            # self.html.LoadPage(path)
+        # dlg.Destroy()
 
     def __OnBack(self, event):
         """
@@ -129,9 +128,7 @@ class DlgHelp(wx.Dialog):
         @author C.Dutoit
         """
         if not self.html.HistoryBack():
-            wx.MessageBox(_("No more items in history !"))
-
-    #>------------------------------------------------------------------------
+            MessageBox(_("No more items in history !"))
 
     def __OnForward(self, event):
         """
@@ -141,9 +138,7 @@ class DlgHelp(wx.Dialog):
         @author C.Dutoit
         """
         if not self.html.HistoryForward():
-            wx.MessageBox(_("No more items in history !"))
-
-    #>------------------------------------------------------------------------
+            MessageBox(_("No more items in history !"))
 
     def __OnViewSource(self, event):
         """
@@ -152,14 +147,11 @@ class DlgHelp(wx.Dialog):
         @since 1.1
         @author C.Dutoit
         """
-        from wx.Python.lib.dialogs import ScrolledMessageDialog
+        #  from wx.Python.lib.dialogs import ScrolledMessageDialog
         source = self.html.GetParser().GetSource()
-        dlg = wx.ScrolledMessageDialog(self, source, _('HTML Source'))
+        dlg = ScrolledMessageDialog(self, source, _('HTML Source'))
         dlg.ShowModal()
         dlg.Destroy()
-
-
-    #>------------------------------------------------------------------------
 
     def __OnPrint(self, event):
         """
@@ -169,16 +161,3 @@ class DlgHelp(wx.Dialog):
         @author C.Dutoit
         """
         self.printer.PrintFile(self.html.GetOpenedPage())
-
-
-
-#Dialog box test
-if __name__ == "__main__":
-    class App(wx.App):
-        def OnInit(self):
-            frame = wx.Frame(None, -1, "Pyut Help test application")
-            self.SetTopWindow(frame)
-            self.dlg=DlgHelp(frame,  -1, "Pyut Help")
-            return True
-    app = App(0)
-    app.MainLoop()
