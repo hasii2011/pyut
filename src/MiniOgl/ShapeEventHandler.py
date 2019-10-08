ff8e9111c7b17dfef0312a08a4de6eb0843b1dd8
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-10-08 07:42:18 -0600

    https://github.com/hasii2011/PyUt/issues/19

--- src/MiniOgl/ShapeEventHandler.py
+++ src/MiniOgl/ShapeEventHandler.py
@@ -1,148 +1,102 @@
-#!/usr/bin/env python
-#
-# Copyright 2002, Laurent Burgbacher, Eivd.
-# Visit http://www.eivd.ch
-#
-# This file is part of MiniOgl.
-#
-# MiniOgl is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# MiniOgl is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with MiniOgl; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__author__    = "Laurent Burgbacher, lb@alawa.ch, Eivd"
-__copyright__ = "Copyright 2002, Laurent Burgbacher, Eivd"
-__license__   = "Released under the terms of the GNU General Public Licence V2"
-__date__      = "2002-10-15"
-__version__   = "$Id: ShapeEventHandler.py,v 1.3 2004/06/16 19:33:18 dutoitc Exp $"
-
+from wx import MouseEvent
 
 __all__ = ["ShapeEventHandler"]
+
 DEBUG = 0
 
 
-class ShapeEventHandler(object):
+class ShapeEventHandler:
     """
     Let a shape receive mouse events directly.
 
-    Exported methods:
-    -----------------
-
-    OnLeftDown(self, event)
-        Callback for left clicks.
-    OnLeftUp(self, event)
-        Callback for left clicks.
-    OnLeftDClick(self, event)
-        Callback for left double clicks.
-    OnMiddleDown(self, event)
-        Callback for middle clicks.
-    OnMiddleUp(self, event)
-        Callback for middle clicks.
-    OnMiddleDClick(self, event)
-        Callback for middle double clicks.
-    OnRightDown(self, event)
-        Callback for right clicks.
-    OnRightUp(self, event)
-        Callback for right clicks.
-    OnRightDClick(self, event)
-        Callback for right double clicks.
-
     @author Laurent Burgbacher <lb@alawa.ch>
     """
-    def OnLeftDown(self, event):
+    def OnLeftDown(self, event: MouseEvent):
         """
         Callback for left clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled left down")
         event.Skip()
 
-    def OnLeftUp(self, event):
+    def OnLeftUp(self, event: MouseEvent):
         """
         Callback for left clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled left up")
         event.Skip()
 
-    def OnLeftDClick(self, event):
+    def OnLeftDClick(self, event: MouseEvent):
         """
         Callback for left double clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled left double click")
         event.Skip()
 
-    def OnMiddleDown(self, event):
+    def OnMiddleDown(self, event: MouseEvent):
         """
         Callback for middle clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled middle down")
         event.Skip()
 
-    def OnMiddleUp(self, event):
+    def OnMiddleUp(self, event: MouseEvent):
         """
         Callback for middle clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled middle up")
         event.Skip()
 
-    def OnMiddleDClick(self, event):
+    def OnMiddleDClick(self, event: MouseEvent):
         """
         Callback for middle double clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled middle double click")
         event.Skip()
 
-    def OnRightDown(self, event):
+    def OnRightDown(self, event: MouseEvent):
         """
         Callback for right clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled right down")
         event.Skip()
 
-    def OnRightUp(self, event):
+    def OnRightUp(self, event: MouseEvent):
         """
         Callback for right clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled right up")
         event.Skip()
 
-    def OnRightDClick(self, event):
+    def OnRightDClick(self, event: MouseEvent):
         """
         Callback for right double clicks.
 
-        @param wx.Event event
+        @param  event
         """
         if DEBUG:
             print("Unhandled right double click")
