ce9844db034712f0c45ed6629d72edec955042fa
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-10-10 09:39:10 -0600

    Now, I understand a little bit more of MiniOgl
    https://github.com/hasii2011/PyUt/issues/2

--- src/tests/TestMiniOgl.py
+++ src/tests/TestMiniOgl.py
@@ -1,54 +1,68 @@
-from DiagramFrame import DiagramFrame
-from PointShape import PointShape
-from RectangleShape import RectangleShape
-from AnchorPoint import AnchorPoint
-from LineShape import LineShape
-import wx
+from wx import App
+from wx import CURSOR_PENCIL
+from wx import Cursor
+from wx import DEFAULT_FRAME_STYLE
+from wx import Frame
+
+from MiniOgl.DiagramFrame import DiagramFrame
+from MiniOgl.Diagram import Diagram
+
+from MiniOgl.PointShape import PointShape
+from MiniOgl.RectangleShape import RectangleShape
+from MiniOgl.AnchorPoint import AnchorPoint
+from MiniOgl.LineShape import LineShape
 
 
-class MyApp(wx.App):
+class TestMiniOglApp(App):
+
+    FRAME_ID: int = 0xDeadBeef
+
     def OnInit(self):
-        frameTop = wx.Frame(None, -1, "", size=(400, 400))
+
+        frameTop: Frame = Frame(parent=None, id=TestMiniOglApp.FRAME_ID, titlr="Test MiniOgl", size=(400, 400), style=DEFAULT_FRAME_STYLE)
         frameTop.Show(True)
-        frame = DiagramFrame(frameTop)
-        frame.SetSize((1200, 1200))
-        frame.SetScrollbars(10, 10, 100, 100)
-        frame.SetCursor(wx.StockCursor(wx.CURSOR_PENCIL))
-        frame.Show(True)
-        self.SetTopWindow(frame)
-        self._frame = frame
-    
+
+        diagFrame: DiagramFrame = DiagramFrame(frameTop)
+        diagFrame.SetSize((1200, 1200))
+        diagFrame.SetScrollbars(10, 10, 100, 100)
+        diagFrame.SetCursor(Cursor(CURSOR_PENCIL))
+        diagFrame.Show(True)
+
+        self.SetTopWindow(diagFrame)
+
+        self._diagFrame: DiagramFrame = diagFrame
+
         self.initTest()
 
         return True
 
-
     def initTest(self):
-        diag = self._frame.GetDiagram()
 
-        shape = PointShape(50, 50)
-        diag.AddShape(shape)
+        diagFrame: Diagram = self._diagFrame.GetDiagram()
+
+        pointShape: PointShape = PointShape(50, 50)
+        diagFrame.AddShape(pointShape)
 
         for x in range(10):
             for y in range(3):
-                shape = PointShape(300 + x*50, 300 + y*50)
-                diag.AddShape(shape)
+                pointShape: PointShape = PointShape(300 + x*50, 300 + y*50)
+                diagFrame.AddShape(pointShape)
+
+        rectShape: RectangleShape = RectangleShape(100, 50, 130, 80)
+        rectShape.SetDraggable(True)
+        diagFrame.AddShape(rectShape)
+
+        anchor1 = AnchorPoint(50, 100)
+        anchor1.SetDraggable(True)
+        anchor2 = AnchorPoint(70, 130)
+        anchor2.SetDraggable(True)
+
+        lineShape: LineShape = LineShape(anchor1, anchor2)
+        lineShape.SetDrawArrow(False)
+        lineShape.SetDraggable(True)
+        diagFrame.AddShape(lineShape)
 
 
-        shape = RectangleShape(100, 50, 130, 80)
-        shape.SetDraggable(True)
-        diag.AddShape(shape)
+testApp: App = TestMiniOglApp(redirect=False)
 
-        anc1 = AnchorPoint(50, 100)
-        anc1.SetDraggable(True)
-        anc2 = AnchorPoint(70, 130)
-        anc2.SetDraggable(True)
-        shape = LineShape(anc1, anc2)
-        shape.SetDrawArrow(False)
-        shape.SetDraggable(True)
-        diag.AddShape(shape)
-
-        
-
-app = MyApp(0)
-app.MainLoop()
+testApp.MainLoop()
