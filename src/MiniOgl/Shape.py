4e18b490c6a2fdeb271bb2ccf19a8cde7a624e98
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-11-15 20:44:49 -0700

    Fixed:
    https://github.com/hasii2011/PyUt/issues/26

--- src/MiniOgl/Shape.py
+++ src/MiniOgl/Shape.py
@@ -1,122 +1,24 @@
+
+from typing import Union
+
+from logging import Logger
+from logging import getLogger
+from logging import DEBUG as pythonDebugLoggingLevel
+
 import wx
 from MiniOgl.ShapeModel import *
+from MiniOgl.TextShape import TextShape
 
 
-__all__ = ["Shape"]
-
-# Setting this to 1 will display a little label for each Shape.
-DEBUG = 0
-
-
-class Shape(object):
+class Shape:
     """
     Shape is the basic graphical block. It is also the view in
     a MVC pattern, so it has has a relative model (ShapeModel).
 
-    Exported methods:
-    -----------------
-
-    __init__(self, x=0.0, y=0.0, parent=None)
-        Constructor.
-    SetPen(self, pen)
-        Set the pen used to draw the shape.
-    GetPen(self)
-        Get the pen used to draw the shape.
-    SetBrush(self, brush)
-        Set the brush used to draw the shape.
-    GetBrush(self)
-        Get the brush used to draw the shape.
-    GetID(self)
-        Get the ID number of the shape.
-    SetID(self, id)
-        Set the ID of the shape. This can be used when loading a shape.
-    SetOrigin(self, x, y)
-        Set the origin of the shape, from its upper left corner.
-    GetOrigin(self)
-        Get the origin of the shape, from its upper left corner.
-    AppendChild(self, child)
-        Append a child to this shape.
-    GetAllChildren(self)
-        Get all the children of this shape, recursively.
-    GetChildren(self)
-        Get the children of this shape.
-    AddAnchor(self, x, y, anchorType=None)
-        Add an anchor point to the shape.
-    AddAnchorObject(self, anchor)
-        Add an anchor point directly.
-    RemoveAllAnchors(self)
-        Remove all anchors of the shape.
-    RemoveAnchor(self, anchor)
-        Remove an anchor.
-    AddText(self, x, y, text)
-        Add a text shape to the shape.
-    Attach(self, diagram)
-        Don't use this method, use Diagram.AddShape instead !!!
-    Detach(self)
-        Detach the shape from its diagram.
-    Draw(self, dc, withChildren=True)
-        Draw the shape.
-    DrawChildren(self, dc)
-        Draw the children of this shape.
-    DrawBorder(self, dc)
-        Draw the border of the shape, for fast rendering.
-    DrawAnchors(self, dc)
-        Draw the anchors of the shape.
-    DrawHandles(self, dc)
-        Draw the handles (selection points) of the shape.
-    GetAnchors(self)
-        Return a list of the anchors of the shape.
-    GetParent(self)
-        Get the parent of this shape.
-    SetParent(self, parent)
-        Set the parent of this shape.
-    GetPosition(self)
-        Return the absolute position of the shape.
-    GetTopLeft(self)
-        Get the coords of the top left point in diagram coords.
-    GetSize(self)
-        Get the size of the shape.
-    ConvertCoordToRelative(self, x, y)
-        Convert absolute coordinates to relative ones.
-    GetRelativePosition(self)
-        Return the position of the shape, relative to it's parent.
-    Inside(self, x, y)
-        True if (x, y) is inside the shape.
-    IsDraggable(self)
-        True if the shape can be dragged.
-    IsProtected(self)
-        True if the shape is protected.
-    IsSelected(self)
-        True if the shape is selected.
-    IsVisible(self)
-        True if the shape is visible.
-    SetSelected(self, state=True)
-        Select the shape.
-    IsMoving(self)
-        Return the "moving" state of a shape.
-    SetMoving(self, state)
-        A non-moving shape will be redrawn faster when others are moved.
-    SetDraggable(self, drag)
-        If False, the shape won't be movable.
-    SetPosition(self, x, y)
-        Change the position of the shape, if it's draggable.
-    SetRelativePosition(self, x, y)
-        Set the position of the shape, relative to the parent.
-    SetProtected(self, bool)
-        Protect the shape against deletion (Detach).
-    SetSize(self, w, h)
-        Set the size of the shape.
-    SetVisible(self, bool)
-        Set the shape visible or not.
-    __repr__(self)
-        String representation.
-    GetDiagram(self)
-        Return the diagram associated with this shape.
-
     @author Laurent Burgbacher <lb@alawa.ch>
     """
 
-    ID = 0 # internal ID number
+    ID = 0  # internal ID number
 
     def __init__(self, x=0.0, y=0.0, parent=None):
         """
@@ -124,8 +26,11 @@ class Shape(object):
         If a parent is given, the position is relative to the parent's
         origin.
 
-        @param double x, y : position of the shape on the diagram
+        @param  x : position of the shape on the diagram
+        @param  y : position of the shape on the diagram
         """
+        self.logger: Logger = getLogger(__name__)
+
         self._x = x                 # shape position (view)
         self._y = y                 # shape position (view)
         self._ox = 0.0              # origin position (view)
@@ -140,26 +45,26 @@ class Shape(object):
         self._protected = False     # to protect against deletion
         self._children = []         # children shapes
         self._privateChildren = []  # private children, not saved
-        self._pen = wx.BLACK_PEN     # pen to use
-        self._brush = wx.WHITE_BRUSH # brush to use
+        self._pen = wx.BLACK_PEN      # pen to use
+        self._brush = wx.WHITE_BRUSH  # brush to use
 
         #  added by P. Dabrowski <przemek.dabrowski@destroy-display.com> (12.11.2005)
-        self._model = ShapeModel(self) # model of the shape (MVC pattern)
+        self._model = ShapeModel(self)  # model of the shape (MVC pattern)
 
         self._id = Shape.ID     # unique ID number
         Shape.ID += 1
-        if DEBUG:
+        if self.logger.level == pythonDebugLoggingLevel:
             from MiniOgl.TextShape import TextShape
             from MiniOgl.LineShape import LineShape
             if not isinstance(self, (TextShape, LineShape)):
-                t = self.AddText(0, -10, str(self._id))
+                t: Union[TextShape, LineShape] = self.AddText(0, -10, str(self._id))
                 t.SetColor(wx.RED)
 
-    def SetPen(self, pen):
+    def SetPen(self, pen: wx.Pen):
         """
         Set the pen used to draw the shape.
 
-        @param wx.Pen pen
+        @param pen
         """
         self._pen = pen
 
@@ -171,11 +76,11 @@ class Shape(object):
         """
         return self._pen
 
-    def SetBrush(self, brush):
+    def SetBrush(self, brush: wx.Brush):
         """
         Set the brush used to draw the shape.
 
-        @param wx.Brush brush
+        @param brush
         """
         self._brush = brush
 
@@ -195,20 +100,21 @@ class Shape(object):
         """
         return self._id
 
-    def SetID(self, id):
+    def SetID(self, theId):
         """
         Set the ID of the shape. This can be used when loading a shape.
 
-        @param id
+        @param theId
         """
-        self._id = id
+        self._id = theId
         Shape.ID = max(self._id + 1, Shape.ID)
 
-    def SetOrigin(self, x, y):
+    def SetOrigin(self, x: float, y: float):
         """
         Set the origin of the shape, from its upper left corner.
 
-        @param double x, y : new origin
+        @param  x  new origin
+        @param  y new origin
         """
         self._ox, self._oy = x, y
 
@@ -220,12 +126,12 @@ class Shape(object):
         """
         return self._ox, self._oy
 
-    def AppendChild(self, child):
+    def AppendChild(self, child: "Shape"):
         """
         Append a child to this shape.
         This doesn't add it to the diagram, but it will be drawn by the parent.
 
-        @param Shape child
+        @param  child
         """
         child.SetParent(self)
         self._children.append(child)
@@ -259,10 +165,10 @@ class Shape(object):
         shape and move with it. It is protected against deletion (by default)
         and not movable by itself.
 
-        @param double x, y : position of the new point, relative to the
-            origin of the shape
-        @param class anchorType : class to use as anchor point, or None for
-            default (AnchorPoint)
+        @param double x : position of the new point, relative to the origin of the shape
+        @param double y : position of the new point, relative to the origin of the shape
+        @param class anchorType : class to use as anchor point, or None for default (AnchorPoint)
+
         @return AnchorPoint : the created anchor
         """
         from MiniOgl.AnchorPoint import AnchorPoint     # I don't like in module imports but there is a cyclical dependency somewhere
@@ -297,18 +203,20 @@ class Shape(object):
         """
         Remove an anchor.
 
-        @param AnchorPoint anchor
+        @param  anchor
         """
         if anchor in self._anchors:
             self._anchors.remove(anchor)
 
-    def AddText(self, x, y, text):
+    def AddText(self, x: float, y: float, text: str) -> TextShape:
         """
         Add a text shape to the shape.
 
-        @param double x, y : position of the text, relative to the
-            origin of the shape
+        @param  x : position of the text, relative to the origin of the shape
+        @param  y : position of the text, relative to the origin of the shape
+
         @param String text : text to add
+
         @return TextShape : the created shape
         """
         t = self._CreateTextShape(x, y, text)
@@ -329,21 +237,21 @@ class Shape(object):
         self._privateChildren.append(t)
         return t
 
-    def _CreateTextShape(self, x, y, text):
+    def _CreateTextShape(self, x, y, text) -> TextShape:
         """
         Create a text shape and add it to the diagram.
 
         @param double x, y : position of the text, relative to the
             origin of the shape
         @param String text : text to add
+
         @return TextShape : the created shape
         """
-        from MiniOgl.TextShape import TextShape
+        # from MiniOgl.TextShape import TextShape
         t = TextShape(x, y, text, self)
         if self._diagram is not None:
             self._diagram.AddShape(t)
-        if DEBUG:
-            print("Text", t, "added")
+        self.logger.debug(f"Text: {t} added")
         return t
 
     def Attach(self, diagram):
@@ -353,7 +261,7 @@ class Shape(object):
         When you create a new shape, you must attach it to a diagram before
         you can see it. This method is used internally by Diagram.AddShape.
 
-        @param Diagram diagram
+        @param  diagram
         """
         self._diagram = diagram
         # add the anchors and the children
@@ -367,14 +275,10 @@ class Shape(object):
         removed, and link lines too.
         """
         # do not detach a protected shape
-        if DEBUG:
-            print("In shape.Detach with", self)
+        self.logger.debug(f"In shape.Detach with: {self}")
         if self._diagram is not None and not self._protected:
-            if DEBUG:
-                print("passed first condition")
+            self.logger.debug(f"passed first condition")
 
-            #  added by P. Dabrowski to ensure that the model is not
-            #  attached anymore to this view.
             self.GetModel()._views.remove(self)
 
             diagram = self._diagram
@@ -390,10 +294,10 @@ class Shape(object):
                 child.SetProtected(False)
                 child.Detach()
                 child.SetProtected(True)
-            if DEBUG:
-                print("now, the shapes are", diagram.GetShapes())
 
-    def Draw(self, dc, withChildren=True):
+            self.logger.debug("now, the shapes are", diagram.GetShapes())
+
+    def Draw(self, dc: wx.DC, withChildren: bool = True):
         """
         Draw the shape.
         For a shape, only the anchors are drawn. Nothing is drawn if the
@@ -402,14 +306,9 @@ class Shape(object):
         parent's Draw method, passing withChildren = False, and finally
         calling itself the DrawChildren method.
 
-        @param wx.DC dc
+        @param dc
         @param boolean withChildren : draw the children or not
         """
-        #C.Dutoit debug
-        #print withChildren
-        #import traceback
-        #traceback.print_stack()
-        #print "-----------------"
 
         if self._visible:
             dc.SetPen(self._pen)
@@ -418,14 +317,14 @@ class Shape(object):
                 self.DrawChildren(dc)
 
         if self._selected:
-            dc.SetPen(wx.RED_PEN) # CD
+            dc.SetPen(wx.RED_PEN)
             self.DrawHandles(dc)
 
     def DrawChildren(self, dc):
         """
         Draw the children of this shape.
 
-        @param wx.DC dc
+        @param dc
         """
         if self._visible:
             for child in self._children + self._anchors + self._privateChildren:
@@ -435,7 +334,7 @@ class Shape(object):
         """
         Draw the border of the shape, for fast rendering.
 
-        @param wx.DC dc
+        @param dc
         """
         pass
 
@@ -443,17 +342,17 @@ class Shape(object):
         """
         Draw the anchors of the shape.
 
-        @param wx.DC dc
+        @param dc
         """
         #  print "Shape.DrawAnchors; shape=", self, ", anchors=", self._anchors
         map(lambda x: x.Draw(dc), self._anchors)
 
-    def DrawHandles(self, dc):
+    def DrawHandles(self, dc: wx.DC):
         """
         Draw the handles (selection points) of the shape.
         A shape has no handles, because it has no size.
 
-        @param wx.DC dc
+        @param  dc
         """
         pass
 
@@ -473,11 +372,11 @@ class Shape(object):
         """
         return self._parent
 
-    def SetParent(self, parent):
+    def SetParent(self, parent: 'Shape'):
         """
         Set the parent of this shape.
 
-        @param Shape parent
+        @param parent
         """
         self._parent = parent
 
@@ -611,11 +510,12 @@ class Shape(object):
         """
         self._draggable = drag
 
-    def SetPosition(self, x, y):
+    def SetPosition(self, x: float, y: float):
         """
         Change the position of the shape, if it's draggable.
 
-        @param double x, y : new position
+        @param x
+        @param y
         """
         if self._draggable:
             if self._parent is not None:
@@ -624,55 +524,49 @@ class Shape(object):
                 self._x = x
                 self._y = y
 
-            #added by P. Dabrowski <przemek.dabrowski@destroy-display.com> (12.11.2005)
-            # if the shape is attached to a diagramFrame, it means that
-            # the model will be initialized correctly.
-            #(Avoid a null pointer error).
+            # added by P. Dabrowski <przemek.dabrowski@destroy-display.com> (12.11.2005)
+            #  if the shape is attached to a diagramFrame, it means that
+            #  the model will be initialized correctly.
+            # (Avoid a null pointer error).
             if self.HasDiagramFrame():
                 self.UpdateModel()
 
-    def SetRelativePosition(self, x, y):
+    def SetRelativePosition(self, x: float, y: float):
         """
         Set the position of the shape, relative to the parent.
         Only works if the shape is draggable.
 
-        @param double x, y : new position
+        @param x
+        @param y
         """
         if self._draggable:
             self._x = x
             self._y = y
 
-    def SetProtected(self, bool):
+    def SetProtected(self, newValue: bool):
         """
         Protect the shape against deletion (Detach).
 
-        @param bool bool
+        @param newValue
         """
-        self._protected = bool
+        self._protected = newValue
 
-    def SetSize(self, w, h):
+    def SetSize(self, w: float, h: float):
         """
         Set the size of the shape.
 
-        @param double w, h : width and height of the shape
+        @param  w, width
+        @param h : height of the shape
         """
         pass
 
-    def SetVisible(self, bool):
+    def SetVisible(self, theNewValue: bool):
         """
         Set the shape visible or not.
 
-        @param bool bool
+        @param theNewValue
         """
-        self._visible = bool
-
-    def __repr__(self):
-        """
-        String representation.
-
-        @return String
-        """
-        return object.__repr__(self) + " : " + str(self._id)
+        self._visible = theNewValue
 
     def GetDiagram(self):
         """
@@ -693,20 +587,19 @@ class Shape(object):
         # Get the coords of the model (ShapeModel)
         mx, my = self.GetModel().GetPosition()
 
-        #Get the offsets and the ratio between the shape (view) and the
-        #shape model (ShapeModel) given by the frame where the shape
-        #is displayed.
+        # Get the offsets and the ratio between the shape (view) and the
+        # shape model (ShapeModel) given by the frame where the shape
+        # is displayed.
         ratio = self.GetDiagram().GetPanel().GetCurrentZoom()
         dx = self.GetDiagram().GetPanel().GetXOffset()
         dy = self.GetDiagram().GetPanel().GetYOffset()
 
-        #calculation of the shape (view) coords in the light of the
-        #offsets and ratio
+        # calculation of the shape (view) coords in the light of the offsets and ratio
         x = (ratio * mx) + dx
         y = (ratio * my) + dy
 
-        #assign the new coords to the shape (view). DON'T USE SetPosition(),
-        #because there is a call to UpdateModel() in that method.
+        # assign the new coords to the shape (view). DON'T USE SetPosition(),
+        # because there is a call to UpdateModel() in that method.
         if self._parent is not None:
             self._x, self._y = self.ConvertCoordToRelative(x, y)
         else:
@@ -723,9 +616,9 @@ class Shape(object):
         #  get the associated model (ShapeModel)
         model = self.GetModel()
 
-        #Get the offsets and the ratio between the shape (view) and the
-        #shape model (ShapeModel) given by the frame where the shape
-        #is displayed.
+        # Get the offsets and the ratio between the shape (view) and the
+        # shape model (ShapeModel) given by the frame where the shape
+        # is displayed.
         ratio = self.GetDiagram().GetPanel().GetCurrentZoom()
         dx = self.GetDiagram().GetPanel().GetXOffset()
         dy = self.GetDiagram().GetPanel().GetYOffset()
@@ -733,16 +626,16 @@ class Shape(object):
         #  get the coords of this shape
         x, y = self.GetPosition()
 
-        #calculation of the model coords in the light of the
-        #offsets and ratio and assignement.
+        # calculation of the model coords in the light of the
+        # offsets and ratio and assignement.
         mx = (x - dx)/ratio
         my = (y - dy)/ratio
         model.SetPosition(mx, my)
 
-        #change also the position of the model of the children,
-        #because when we move the parent children setposition isn't called
-        #and so their updatemodel isn't called
-        for child in self._anchors : #  + self.GetAllChildren():
+        # change also the position of the model of the children,
+        # because when we move the parent children setposition isn't called
+        # and so their updatemodel isn't called
+        for child in self._anchors:        # + self.GetAllChildren():
             cx, cy = child.GetPosition()
             cmx = (cx - dx) / ratio
             cmy = (cy - dy) / ratio
@@ -775,3 +668,11 @@ class Shape(object):
             return self.GetDiagram().GetPanel() is not None
         else:
             return False
+
+    def __repr__(self):
+        """
+        String representation.
+
+        @return String
+        """
+        return object.__repr__(self) + " : " + str(self._id)
