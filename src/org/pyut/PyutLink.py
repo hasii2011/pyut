42497199585dcd1738ed33e95e3a6eae45ec1111
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2020-01-13 16:08:41 -0700

    https://github.com/hasii2011/PyUt/issues/13
    Prep the class so I can understand it in the dialog

--- src/org/pyut/PyutLink.py
+++ src/org/pyut/PyutLink.py
@@ -2,6 +2,7 @@
 from logging import Logger
 from logging import getLogger
 
+from org.pyut.PyutLinkedObject import PyutLinkedObject
 from org.pyut.PyutObject import PyutObject
 from org.pyut.enums.OglLinkType import OglLinkType
 
@@ -14,88 +15,75 @@ class PyutLink(PyutObject):
 
     A PyutLink represents a UML link between Class in Pyut.
 
-    Example::
-        myLink  = PyutLink("linkName", 0, "0", "*")
+    Example:
+    ```python
 
-    :version: $Revision: 1.5 $
-    :author: Deve Roux
-    :contact: droux@eivd.ch
+        myLink  = PyutLink("linkName", OglLinkType.OGL_INHERITANCE, "0", "*")
+    ```
     """
 
-    def __init__(self, name="", linkType: OglLinkType = OglLinkType.OGL_INHERITANCE, cardSrc="", cardDest="", bidir=0, source=None, destination=None):
+    def __init__(self, name="", linkType: OglLinkType = OglLinkType.OGL_INHERITANCE,
+                 cardSrc: str = "", cardDest: str = "",
+                 bidir: bool = False, source: PyutLinkedObject = None, destination: PyutLinkedObject = None):
         """
-        Constructor.
 
-        @param string name     : for the link name
-        @param int    linkType     : type of link
-        @param string  cardSrc : cardinality of source of link
-        @param string  cardDest: cardinality of destination of link
-        @param boolean bidir   : design if link is bidirational or not
-        @param obj source      : source of the link
-        @param obj destination : where goes the link
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
-        @modified Laurent Burgbacher <lb@alawa.ch>
-            added source field
+        Args:
+            name:        The link name
+            linkType:    The enum represening the link type
+            cardSrc:     The source cardinality
+            cardDest:    The destination cardinality
+            bidir:      `True` if the link is bidirectional, else `False`
+            source:      The source of the link
+            destination: The destination of the link
         """
-        # PyutObject.__init__(self, name)
         super().__init__(name)
         self.logger: Logger       = getLogger(__name__)
         self._type:  OglLinkType = linkType
 
-        self._cardSrc = cardSrc
-        self._cardDes = cardDest
-        self._bidir   = bidir
-        self._src     = source
-        self._dest    = destination
+        self._sourceCardinality:      str  = cardSrc
+        self._destinationCardinality: str  = cardDest
+        self._bidirectional:          bool = bidir
+
+        self._src:  PyutLinkedObject = source
+        self._dest: PyutLinkedObject = destination
 
     def getSrcCard(self) -> str:
         """
         Return a string representing cardinality source.
 
         @return string source cardinality
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        return self._cardSrc
+        return self._sourceCardinality
 
     def setSrcCard(self, cardSrc: str):
         """
         Updating source cardinality.
 
         @param  cardSrc
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        self._cardSrc = cardSrc
+        self._sourceCardinality = cardSrc
 
     def getDestCard(self):
         """
         Return a string representing cardinality destination.
 
         @return string destination cardinality
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        return self._cardDes
+        return self._destinationCardinality
 
     def setDestCard(self, cardDest: str):
         """
         Updating destination cardinality.
 
         @param cardDest
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        self._cardDes = cardDest
+        self._destinationCardinality = cardDest
 
     def getSource(self):
         """
         Return the source object of the link
 
         @return object Class or Note
-        @since 1.0
-        @author Laurent Burgbacher <lb@alawa.ch>
         """
         return self._src
 
@@ -104,8 +92,6 @@ class PyutLink(PyutObject):
         Set the source object of this link.
 
         @param  source  PyutClass or PyutNote
-        @since 1.0
-        @author Laurent Burgbacher <lb@alawa.ch>
         """
         self._src = source
 
@@ -114,8 +100,6 @@ class PyutLink(PyutObject):
         Return an object destination who is linked to this link.
 
         @return object Class or Note
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
         return self._dest
 
@@ -124,8 +108,6 @@ class PyutLink(PyutObject):
         Updating destination.
 
         @param destination -- PyutClass or PyutNote
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
         self._dest = destination
 
@@ -134,20 +116,16 @@ class PyutLink(PyutObject):
         To know if the link is bidirectional.
 
         @return boolean
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        return self._bidir
+        return self._bidirectional
 
     def setBidir(self, bidirectional: bool):
         """
         Updating bidirectional.
 
         @param bidirectional
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
-        self._bidir = bidirectional
+        self._bidirectional = bidirectional
 
     def setType(self, theType: OglLinkType):
         """
@@ -170,7 +148,6 @@ class PyutLink(PyutObject):
         To get the link type.
 
         @return  : The type of the link
-
         """
         return self._type
 
@@ -179,7 +156,5 @@ class PyutLink(PyutObject):
         String representation.
 
         @return : string representing link
-        @since 1.0
-        @author Deve Roux <droux@eivd.ch>
         """
         return _(f'("{self.getName()}") links from {self._src} to {self._dest}')
