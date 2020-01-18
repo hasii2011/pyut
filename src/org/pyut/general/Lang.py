44b2f46b96a1d97ba896b41ce03ad3e3371466ad
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2020-01-18 10:06:32 -0700

    Prep work
    https://github.com/hasii2011/PyUt/issues/62

--- src/org/pyut/general/Lang.py
+++ src/org/pyut/general/Lang.py
@@ -4,7 +4,16 @@ from logging import getLogger
 
 import gettext
 
-import wx
+from wx import LANGUAGE_CHINESE_TRADITIONAL
+from wx import LANGUAGE_DANISH
+from wx import LANGUAGE_ENGLISH
+from wx import LANGUAGE_ESPERANTO
+from wx import LANGUAGE_FRENCH
+from wx import LANGUAGE_GERMAN
+from wx import LANGUAGE_INDONESIAN
+from wx import LANGUAGE_POLISH
+from wx import LANGUAGE_SPANISH
+from wx import Locale
 
 from org.pyut.PyutPreferences import PyutPreferences
 
@@ -14,15 +23,15 @@ from org.pyut.errorcontroller.ErrorManager import ErrorManager
 DEFAULT_LANG = "en"
 LANGUAGES = {
         DEFAULT_LANG: (
-            "English",              wx.LANGUAGE_ENGLISH),
-            "da": ("Danish",        wx.LANGUAGE_DANISH),
-            "de": ("Deutsch",       wx.LANGUAGE_GERMAN),
-            "eo": ("Esperanto",     wx.LANGUAGE_ESPERANTO),
-            "es": ("Spanish",       wx.LANGUAGE_SPANISH),
-            "fr": ("Francais",      wx.LANGUAGE_FRENCH),
-            "id": ("Indonesian",    wx.LANGUAGE_INDONESIAN),
-            "pl": ("Polish",        wx.LANGUAGE_POLISH),
-            "ci": ("chinese-utf8",  wx.LANGUAGE_CHINESE_TRADITIONAL),
+            "English",              LANGUAGE_ENGLISH),
+            "da": ("Danish",        LANGUAGE_DANISH),
+            "de": ("Deutsch",       LANGUAGE_GERMAN),
+            "eo": ("Esperanto",     LANGUAGE_ESPERANTO),
+            "es": ("Spanish",       LANGUAGE_SPANISH),
+            "fr": ("Francais",      LANGUAGE_FRENCH),
+            "id": ("Indonesian",    LANGUAGE_INDONESIAN),
+            "pl": ("Polish",        LANGUAGE_POLISH),
+            "ci": ("chinese-utf8",  LANGUAGE_CHINESE_TRADITIONAL),
         }
 
 
@@ -41,8 +50,6 @@ def importLanguage():
     # Set language for all application
     moduleLogger.debug(f'Installing language <{language}>')
     try:
-        # Latest test 20050308
-        # langid=wx.LANGUAGE_CHINESE_TRADITIONAL
         wxLangID   = LANGUAGES[language][1]
         domain    = "Pyut"
         localedir = "src"  # "./locale"     TODO: look this up via a resource directory
@@ -52,14 +59,11 @@ def importLanguage():
         if method == 0:
             # Possibility to load all languages, then do an install on fly
             tr = gettext.translation(domain, localedir, languages=[language])
-            # Python 3 update
-            # tr.install(unicode=True)
             tr.install(True)
-            # gettext.install(domain, localedir, unicode=True)
         elif method == 1:
 
             # Set locale for wxWidget
-            loc = wx.Locale(wxLangID)
+            loc = Locale(wxLangID)
             loc.AddCatalogLookupPathPrefix(localedir)
             loc.AddCatalog(domain)
 
