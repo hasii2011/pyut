da818c4ed15f8f42dc70c9a44585def5ec514f4e
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-02-13 13:53:35 -0800

    https://github.com/hasii2011/PyUt/issues/82
    Fix google doc strings before moving it

--- src/org/pyut/PyutModifier.py
+++ src/org/pyut/PyutModifier.py
@@ -3,38 +3,37 @@
 class PyutModifier:
     """
     Modifier for a method or param.
-    These are words like "abstract", "virtual", "const"...
+    These are words like:
+    
+    * "abstract"
+    * "virtual"
+    *"const"...
 
-    :author: Laurent Burgbacher
-    :contact: <lb@alawa.ch>
-    :version: $Revision: 1.4 $
     """
     def __init__(self, modifierTypeName: str = ""):
         """
-        Constructor.
 
-        @param  for the type
-        @since 1.0
-        @author Laurent Burgbacher <lb@alawa.ch>
+        Args:
+            modifierTypeName:   for the type
         """
         self.__name = modifierTypeName
 
-    def getName(self):
+    def getName(self) -> str:
+        """
+
+        Returns:
+             name
+        """
         """
         Get method, used to know the name.
 
-        @return string name
-        @since 1.0
-        @author Laurent Burgbacher <lb@alawa.ch>
+        @return 
         """
         return self.__name
 
     def __str__(self):
         """
-        String representation.
-
-        @return type : string
-        @since 1.0
-        @author Laurent Burgbacher <lb@alawa.ch>
+        Returns:
+            String representation.
         """
         return self.getName()
