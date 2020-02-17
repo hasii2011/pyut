3178dc661af332bd919d2125d8242d39f1966ab5
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-02-17 08:35:46 -0700

    Make unit test script clean up after dirty rotten unit tests

--- runtests.sh
+++ runtests.sh
@@ -5,6 +5,11 @@ cd src/tests
 python3 -m TestAll $*
 status=$?
 
+cd ..
+find . -name pyutHistory"*" -exec rm -rf {} \; -print
+find . -name "*".log -exec rm -rf {} \; -print
+
+
 echo "Exit with status: ${status}"
 exit ${status}
 
