28f9a70e17d77f6481b990f65fee8fdd4df7bb4d
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-02-17 09:34:17 -0700

    Silence is golden;  make sure we do a total cleanup

--- runtests.sh
+++ runtests.sh
@@ -5,9 +5,9 @@ cd src/tests
 python3 -m TestAll $*
 status=$?
 
-cd ..
-find . -name pyutHistory"*" -exec rm -rf {} \; -print
-find . -name "*".log -exec rm -rf {} \; -print
+cd ../..
+find . -name pyutHistory"*" -exec rm -rf {} \; -print   > /dev/null
+find . -name "*".log -exec rm -rf {} \; -print          > /dev/null
 
 
 echo "Exit with status: ${status}"
