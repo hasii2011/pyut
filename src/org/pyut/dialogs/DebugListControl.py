fc0b47198ea8da0e2cbbf1427c3d55cffb975d69
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-04-20 20:40:22 -0500

    https://github.com/hasii2011/PyUt/issues/115
    Move code to appropriate place

--- src/org/pyut/dialogs/DebugListControl.py
+++ src/org/pyut/dialogs/DebugListControl.py
@@ -1,19 +1,124 @@
+from logging import DEBUG
+from logging import ERROR
+from logging import FATAL
+from logging import INFO
+from logging import WARNING
 
+from wx import CheckBox
+from wx import Colour
+from wx import CommandEvent
 from wx import DefaultPosition
+from wx import DefaultSize
+from wx import EVT_CHECKBOX
+from wx import ID_ANY
 
 from wx import LC_REPORT
+from wx import LIST_AUTOSIZE
+from wx import LIST_MASK_FORMAT
+from wx import LIST_MASK_TEXT
 
 from wx.lib.agw import ultimatelistctrl as ULC
+
+from wx.lib.agw.ultimatelistctrl import ULC_HAS_VARIABLE_ROW_HEIGHT
+from wx.lib.agw.ultimatelistctrl import ULC_MASK_CHECK
 from wx.lib.agw.ultimatelistctrl import ULC_REPORT
 from wx.lib.agw.ultimatelistctrl import ULC_SORT_ASCENDING
 from wx.lib.agw.ultimatelistctrl import ULC_VRULES
+from wx.lib.agw.ultimatelistctrl import UltimateListItem
 
 from wx.lib.mixins.listctrl import ListRowHighlighter
 
 
 class DebugListControl(ULC.UltimateListCtrl, ListRowHighlighter):
-    def __init__(self, parent, ID, pos=DefaultPosition, size=(600, 200), agwStyle=ULC_REPORT | ULC_SORT_ASCENDING | ULC_VRULES, style=LC_REPORT):
+
+    IDX_NAME_COLUMN:      int = 0
+    IDX_LEVEL_COLUMN:     int = 1
+    IDX_DISABLED_COLUMN:  int = 2
+    IDX_PROPAGATE_COLUMN: int = 3
+
+    CHECK_BOX_KIND:    int = 1
+    RADIO_BUTTON_KIND: int = 2
+
+    DARK_GRAY:  Colour = Colour(230, 230, 230)
+    LIGHT_GRAY: Colour = Colour(250, 250, 250)
+
+    def __init__(self, parent, ID, pos=DefaultPosition, size=(650, 200),
+                 agwStyle=ULC_REPORT | ULC_HAS_VARIABLE_ROW_HEIGHT | ULC_SORT_ASCENDING | ULC_VRULES,
+                 style=LC_REPORT):
 
         ULC.UltimateListCtrl.__init__(self, parent, ID, pos, size, agwStyle=agwStyle, style=style)
         ListRowHighlighter.__init__(self)
 
+        import logging
+        loggers = [logging.getLogger()]  # get the root logger
+
+        # noinspection PyUnresolvedReferences
+        self._loggers: List[Logger] = loggers + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+
+    def populateList(self):
+
+        defaultMask = LIST_MASK_TEXT | LIST_MASK_FORMAT
+
+        self.InsertColumnInfo(self.IDX_NAME_COLUMN,      self._getColumnHeader("Name", mask=defaultMask))
+        self.InsertColumnInfo(self.IDX_LEVEL_COLUMN,     self._getColumnHeader("Level", mask=defaultMask))
+        self.InsertColumnInfo(self.IDX_DISABLED_COLUMN,  self._getColumnHeader('Disabled?',  self.CHECK_BOX_KIND, defaultMask | ULC_MASK_CHECK))
+        self.InsertColumnInfo(self.IDX_PROPAGATE_COLUMN, self._getColumnHeader('Propagate?', self.CHECK_BOX_KIND, defaultMask | ULC_MASK_CHECK))
+
+        row: int = 0
+
+        for logger in self._loggers:
+
+            self.InsertStringItem(row, logger.name)
+
+            self.SetStringItem(row, self.IDX_LEVEL_COLUMN, self._levelToString(logger.level))
+            self.SetStringItem(row, self.IDX_DISABLED_COLUMN,  '')
+            self.SetStringItem(row, self.IDX_PROPAGATE_COLUMN, '')
+
+            disabledCB: CheckBox = CheckBox(self, ID_ANY, "", DefaultPosition, DefaultSize, 0, name=f'Disabled_{str(row)}_{self.IDX_DISABLED_COLUMN}')
+            disabledCB.SetValue(bool(logger.disabled))
+            disabledCB.Bind(EVT_CHECKBOX, self._onListItemChecked)
+            self.SetItemWindow(row, self.IDX_DISABLED_COLUMN, disabledCB, expand=True)
+
+            propagateCB: CheckBox = CheckBox(self, ID_ANY, "", DefaultPosition, DefaultSize, 0, name=f'Propagate_{str(row)}_{self.IDX_PROPAGATE_COLUMN}')
+            propagateCB.SetValue(bool(logger.propagate))
+            propagateCB.Bind(EVT_CHECKBOX, self._onListItemChecked)
+            self.SetItemWindow(row, self.IDX_PROPAGATE_COLUMN, propagateCB, expand=True)
+
+            row += 1
+
+        self.SetColumnWidth(self.IDX_NAME_COLUMN,      LIST_AUTOSIZE)
+        self.SetColumnWidth(self.IDX_LEVEL_COLUMN,     LIST_AUTOSIZE)
+        self.SetColumnWidth(self.IDX_DISABLED_COLUMN,  LIST_AUTOSIZE)
+        self.SetColumnWidth(self.IDX_PROPAGATE_COLUMN, LIST_AUTOSIZE)
+
+    def _getColumnHeader(self, text: str, kind=0, mask=LIST_MASK_TEXT | LIST_MASK_FORMAT) -> UltimateListItem:
+
+        info: UltimateListItem = UltimateListItem()
+
+        info._format = 0
+        info.SetMask(mask)
+        info.SetText(kind)
+        info.SetText(text)
+
+        return info
+
+    def _levelToString(self, debugLevel: int) -> str:
+
+        levelStr: str = ' NOTSET '
+        if debugLevel == ERROR:
+            levelStr = ' ERROR '
+        elif debugLevel == WARNING:
+            levelStr = ' WARNING '
+        elif debugLevel == INFO:
+            levelStr = ' INFO '
+        elif debugLevel == DEBUG:
+            levelStr = ' DEBUG '
+        elif debugLevel == FATAL:
+            levelStr = ' FATAL '
+
+        return levelStr
+
+    def _onListItemChecked(self, event: CommandEvent):
+
+        print(f'name: {event.GetEventObject().Name} string: {event.GetString()}')
+        # gp: DebugListControl = event.GetEventObject().GetGrandParent()
--- src/org/pyut/dialogs/DlgPyutDebug.py
+++ src/org/pyut/dialogs/DlgPyutDebug.py
@@ -1,14 +1,8 @@
 
-from typing import List
 from typing import Tuple
 
 from logging import Logger
 from logging import getLogger
-from logging import DEBUG
-from logging import WARNING
-from logging import INFO
-from logging import ERROR
-from logging import FATAL
 
 from wx import ALL
 from wx import BORDER_SUNKEN
@@ -16,21 +10,18 @@ from wx import CANCEL
 from wx import CAPTION
 from wx import CENTER
 from wx import CLOSE_BOX
+
 from wx import EVT_BUTTON
 from wx import EVT_CLOSE
 from wx import EVT_SIZE
 from wx import EXPAND
 from wx import OK
 from wx import ID_OK
-from wx import LC_EDIT_LABELS
-from wx import LIST_AUTOSIZE
-
 from wx import VERTICAL
 
 from wx import BoxSizer
 from wx import CommandEvent
 from wx import SizeEvent
-
 
 from org.pyut.PyutUtils import PyutUtils
 
@@ -41,16 +32,12 @@ from org.pyut.dialogs.DebugListControl import DebugListControl
 
 class DlgPyutDebug(BaseDlgEdit):
 
+    SCROLL_BAR_SPACE: int = 6
+
     def __init__(self, theParent, theWindowId):
 
         super().__init__(theParent, theWindowId, "Debug Pyut", theStyle=CLOSE_BOX | CAPTION)
         self.logger: Logger = getLogger(__name__)
-
-        import logging
-        loggers = [logging.getLogger()]  # get the root logger
-
-        # noinspection PyUnresolvedReferences
-        self._loggers: List[Logger] = loggers + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
 
         hs:        BoxSizer = self._createDialogButtonsContainer()
         mainSizer: BoxSizer = BoxSizer(orient=VERTICAL)
@@ -62,12 +49,12 @@ class DlgPyutDebug(BaseDlgEdit):
 
         self.SetSizer(mainSizer)
         mainSizer.Fit(self)
-        mainSizer.InsertSpacer(index=0, size=7)    # magic # for the index
+        mainSizer.InsertSpacer(index=0, size=7)     # magic # for the index
         mainSizer.InsertSpacer(index=2, size=20)    # magic # for the index
         mainSizer.SetSizeHints(self)
         mainSizer.Fit(self)
 
-        self.Bind(EVT_SIZE, self._onSize)
+        self.Bind(EVT_SIZE, self.__onSize)
 
         self.Bind(EVT_BUTTON, self.__OnCmdOk, id=ID_OK)
         self.Bind(EVT_CLOSE,  self.__OnClose)
@@ -76,52 +63,23 @@ class DlgPyutDebug(BaseDlgEdit):
         """
         Initialize the controls.
         """
-        # IDs
-        [
-            self.__xId, self.__tId
-        ] = PyutUtils.assignID(2)
+        [ self.__tId] = PyutUtils.assignID(1)
 
-        dbgListCtrl: DebugListControl = DebugListControl(self, self.__tId, style=BORDER_SUNKEN | LC_EDIT_LABELS)
+        dbgListCtrl: DebugListControl = DebugListControl(self, self.__tId, style=BORDER_SUNKEN)
 
-        self._populateList(dbgListCtrl)
+        dbgListCtrl.populateList()
 
         return dbgListCtrl
 
-    def _populateList(self, dbgListCtrl: DebugListControl):
+    def __onSize(self, event: SizeEvent):
+        """
+        This will only fire once since I don't allow resize of the dialog
+        Args:
+            event:
 
-        dbgListCtrl.InsertColumn(0, "Name")
-        dbgListCtrl.InsertColumn(1, "Level")
-        dbgListCtrl.InsertColumn(2, "Disabled?")
-        dbgListCtrl.InsertColumn(3, "Propagate?")
+        Returns:
 
-        for logger in self._loggers:
-            levelStr: str = self.__levelToString(logger.level)
-            entry = (logger.name, levelStr, str(logger.disabled), str(logger.propagate))
-            dbgListCtrl.Append(entry)
-
-        dbgListCtrl.SetColumnWidth(0, LIST_AUTOSIZE)
-        dbgListCtrl.SetColumnWidth(1, LIST_AUTOSIZE)
-        dbgListCtrl.SetColumnWidth(2, LIST_AUTOSIZE)
-        dbgListCtrl.SetColumnWidth(3, LIST_AUTOSIZE)
-
-    def __levelToString(self, debugLevel: int) -> str:
-
-        levelStr: str = ' NOTSET '
-        if debugLevel == ERROR:
-            levelStr = ' ERROR '
-        elif debugLevel == WARNING:
-            levelStr = ' WARNING '
-        elif debugLevel == INFO:
-            levelStr = ' INFO '
-        elif debugLevel == DEBUG:
-            levelStr = ' DEBUG '
-        elif debugLevel == FATAL:
-            levelStr = ' FATAL '
-
-        return levelStr
-
-    def _onSize(self, event: SizeEvent):
-
+        """
         size: Tuple[int, int] = event.GetSize()
 
         width:    int = size[0]
@@ -131,12 +89,12 @@ class DlgPyutDebug(BaseDlgEdit):
         self.logger.info(f'width: {width} nColumns: {nColumns} colWidth: {colWidth}')
 
         for x in range(nColumns):
-            self._list.SetColumnWidth(x, colWidth)
+            self._list.SetColumnWidth(x, colWidth - DlgPyutDebug.SCROLL_BAR_SPACE)  # Allow room for scroll bar
 
         dlgSize: Tuple[int, int] = self._list.GetSize()
         dlgHeight: int = dlgSize[1]
-        self._list.SetSizeWH(width, dlgHeight)
 
+        self._list.SetSize(width, dlgHeight)
 
     def __OnCmdOk(self, event: CommandEvent):
         """
