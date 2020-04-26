c7cad46e026130488362470ff182be3b578bc698
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-04-25 19:30:52 -0500

    Fix the informal test

--- src/tests/TestMiniOgl.py
+++ src/tests/TestMiniOgl.py
@@ -11,6 +11,7 @@ from org.pyut.MiniOgl.RectangleShape import RectangleShape
 from org.pyut.MiniOgl.AnchorPoint import AnchorPoint
 from org.pyut.MiniOgl.LineShape import LineShape
 from org.pyut.MiniOgl.ControlPoint import ControlPoint
+from org.pyut.PyutPreferences import PyutPreferences
 
 
 class TestMiniOglApp(App):
@@ -18,6 +19,8 @@ class TestMiniOglApp(App):
     FRAME_ID: int = 0xDeadBeef
 
     def OnInit(self):
+
+        PyutPreferences.determinePreferencesLocation()
 
         frameTop: Frame = Frame(parent=None, id=TestMiniOglApp.FRAME_ID, title="Test MiniOgl", size=(400, 400), style=DEFAULT_FRAME_STYLE)
         frameTop.Show(True)
@@ -38,19 +41,19 @@ class TestMiniOglApp(App):
 
     def initTest(self):
 
-        diagFrame: Diagram = self._diagramFrame.GetDiagram()
+        diagramFrame: Diagram = self._diagramFrame.GetDiagram()
 
         pointShape: PointShape = PointShape(50, 50)
-        diagFrame.AddShape(pointShape)
+        diagramFrame.AddShape(pointShape)
 
         for x in range(10):
             for y in range(3):
                 pointShape: PointShape = PointShape(300 + x*50, 300 + y*50)
-                diagFrame.AddShape(pointShape)
+                diagramFrame.AddShape(pointShape)
 
         rectShape: RectangleShape = RectangleShape(100, 50, 130, 80)
         rectShape.SetDraggable(True)
-        diagFrame.AddShape(rectShape)
+        diagramFrame.AddShape(rectShape)
 
         anchor1 = AnchorPoint(50, 100)
         anchor1.SetDraggable(True)
@@ -67,7 +70,7 @@ class TestMiniOglApp(App):
         controlPoint = ControlPoint(200, 150)
         lineShape.AddControl(controlPoint)
 
-        diagFrame.AddShape(lineShape)
+        diagramFrame.AddShape(lineShape)
 
 
 testApp: App = TestMiniOglApp(redirect=False)
