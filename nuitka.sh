1a3f1e430df96078ca326363c3170fd6c869cd29
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-05-01 13:46:16 -0500

    Trying to use nuitka

--- nuitka.sh
+++ nuitka.sh
@@ -6,3 +6,10 @@ export IO_PLUGINS="--include-plugin-directory=src/org/pyut/persistence"
 export PYUT_PLUGINS="--include-plugin-directory=src/org/pyut/plugins"
 
 python3 -m nuitka --clang --standalone ${VERBOSE} ${IMPORTS} src/Pyut.py
+
+cd Pyut.dist
+mkdir -p org/pyut/resources
+cd ..
+
+cp -p src/org/pyut/resources/loggingConfiguration.json Pyut.dist/org/pyut/resources
+cp -p src/org/pyut/resources/Kilroy-Pyut.txt Pyut.dist/org/pyut/resources
