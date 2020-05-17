0ce80f3776385943eb71c051e290d263973ed5d2
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-05-16 20:11:38 -0500

    https://github.com/hasii2011/PyUt/issues/135
    Stage 1

---
+++ src/org/pyut/MiniOgl/Common.py
@@ -0,0 +1,69 @@
+
+from math import sqrt
+
+
+class Common:
+
+    CLICK_TOLERANCE: float = 4.0
+    """
+    A `mix-in` class to share common code currently between a LineShape and a shape that is
+    almost a line.  :-)
+
+    """
+
+    def insideSegment(self, clickDiffStartX: float, clickDiffStartY: float, diffX: float, diffY: float) -> bool:
+        """
+
+        Args:
+            clickDiffStartX:
+            clickDiffStartY:
+            diffX:  Difference between the line's end X coordinate and the start X coordinate
+            diffY:  Difference between the line's end Y coordinate and the start Y coordinate
+
+        Returns: `True` if the click points is withing the click tolerance; else `False`
+        """
+        den: float = sqrt(diffX * diffX + diffY * diffY)
+
+        if den != 0.0:
+            d: float = (clickDiffStartX * diffY - clickDiffStartY * diffX) / den
+        else:
+            return False
+        return abs(d) < Common.CLICK_TOLERANCE
+
+    def insideBoundingBox(self, clickDiffStartX: float, clickDiffStartY: float, diffX: float, diffY: float):
+        """
+        Check if the point (x, y) is inside a box of origin (0, 0) and
+        diagonal (a, b) with a tolerance of CLICK_TOLERANCE
+
+        Args:
+            clickDiffStartX:
+            clickDiffStartY:
+            diffX:  Difference between the line's end X coordinate and the start X coordinate
+            diffY:  Difference between the line's end Y coordinate and the start Y coordinate
+
+        Returns:  `True` if the click point is in the bounding box else `False`
+
+        """
+        ma: float = diffX / 2
+        mb: float = diffY / 2
+
+        if diffX > 0:
+            w: int = max(4, int(diffX - 8))
+        else:
+            w: int = min(-4, int(diffX + 8))
+
+        if diffY > 0:
+            h: int = max(4, int(diffY - 8))
+        else:
+            h: int = min(-4, int(diffY + 8))
+
+        topLeftX: float = ma - w / 2
+        topLeftY: float = mb - h / 2
+
+        i:  bool = clickDiffStartX > topLeftX
+        j:  bool = clickDiffStartX > topLeftX + w
+        k:  bool = clickDiffStartY > topLeftY
+        ll: bool = clickDiffStartY > topLeftY + h
+
+        ans: bool = (i + j) == 1 and (k + ll) == 1
+        return ans
