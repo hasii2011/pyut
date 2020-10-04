b41983b0f5d9aa0f987747c51abcaed348705bd5
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-10-03 20:34:53 -0500

    <Enhancement>[Preferences]: Support Code
    Include minimal unit test
    
    https://github.com/hasii2011/PyUt/issues/196

---
+++ src/org/pyut/miniogl/PyutColorEnum.py
@@ -0,0 +1,50 @@
+
+from enum import Enum
+
+from wx import Colour
+from wx import ColourDatabase
+
+
+class PyutColorEnum(Enum):
+
+    """
+    The purpose of this enumeration is to keep wxPython types from getting into
+    the user visible portions of the preferences dialog
+    """
+
+    BLACK           = 'Black'
+    CORNFLOWER_BLUE = 'Cornflower Blue'
+    WHITE           = 'White'
+    LIGHT_GREY      = 'Light Grey'
+    GREEN           = 'Green'
+    MEDIUM_BLUE     = 'Medium Blue'
+    MIDNIGHT_BLUE   = 'Midnight Blue'
+    YELLOW         = 'Yellow'
+
+    SALMON          = 'Salmon'
+
+    @staticmethod
+    def toWxColor(colorEnum: 'PyutColorEnum') -> Colour:
+        """
+        """
+        cdb: ColourDatabase = ColourDatabase()
+        if colorEnum == PyutColorEnum.BLACK:
+            c: Colour = cdb.Find(PyutColorEnum.BLACK.value)
+        elif colorEnum == PyutColorEnum.CORNFLOWER_BLUE:
+            c: Colour = cdb.Find(PyutColorEnum.CORNFLOWER_BLUE.value)
+        elif colorEnum == PyutColorEnum.LIGHT_GREY:
+            c: Colour = cdb.Find(PyutColorEnum.LIGHT_GREY.value)
+        elif colorEnum == PyutColorEnum.GREEN:
+            c: Colour = cdb.Find(PyutColorEnum.GREEN.value)
+        elif colorEnum == PyutColorEnum.MEDIUM_BLUE:
+            c: Colour = cdb.Find(PyutColorEnum.MEDIUM_BLUE.value)
+        elif colorEnum == PyutColorEnum.MIDNIGHT_BLUE:
+            c: Colour = cdb.Find(PyutColorEnum.MIDNIGHT_BLUE.value)
+        elif colorEnum == PyutColorEnum.YELLOW:
+            c: Colour = cdb.Find(PyutColorEnum.YELLOW.value)
+        else:
+            c: Colour = cdb.Find(PyutColorEnum.WHITE.value)
+
+        del cdb
+        assert c.IsOk(), 'Developer Error.  Invalid color'
+        return c
---
+++ src/tests/TestPyutColorEnum.py
@@ -0,0 +1,73 @@
+
+from logging import Logger
+from logging import getLogger
+
+from unittest import TestSuite
+from unittest import main as unitTestMain
+
+from wx import App
+from wx import C2S_NAME
+from wx import Colour
+
+from org.pyut.miniogl.PyutColorEnum import PyutColorEnum
+
+from tests.TestBase import TestBase
+
+
+class TestPyutColorEnum(TestBase):
+    """
+    """
+    clsLogger: Logger = None
+
+    @classmethod
+    def setUpClass(cls):
+        TestBase.setUpLogging()
+        TestPyutColorEnum.clsLogger = getLogger(__name__)
+
+    def setUp(self):
+        self.logger: Logger = TestPyutColorEnum.clsLogger
+        self.app = App()
+
+    def tearDown(self):
+        del self.app
+
+    def testBlack(self):
+        c: Colour = PyutColorEnum.toWxColor(PyutColorEnum.BLACK)
+        self.assertTrue(c.IsOk(), 'Wah, wah.  Black should be a valid color')
+
+    def testLightGrey(self):
+        c: Colour = PyutColorEnum.toWxColor(PyutColorEnum.LIGHT_GREY)
+        self.assertTrue(c.IsOk(), 'Wah, wah.  Light Grey should be a valid color')
+
+    def testCornFlowerBlue(self):
+        c: Colour = PyutColorEnum.toWxColor(PyutColorEnum.CORNFLOWER_BLUE)
+        self.assertTrue(c.IsOk(), 'Wah, wah.  Corn Flower Blue should be a valid color')
+
+    def testYellow(self):
+        c: Colour = PyutColorEnum.toWxColor(PyutColorEnum.YELLOW)
+        self.assertTrue(c.IsOk(), 'Wah, wah.  Yellow should be a valid color')
+
+    def testUnHandledColor(self):
+        c: Colour = PyutColorEnum.toWxColor(PyutColorEnum.SALMON)
+
+        self.assertTrue(c.IsOk(), 'Wah, wah.  Salmon should get valid color')
+
+        actualName:   str = c.GetAsString(C2S_NAME)
+        expectedName: str = 'white'
+
+        self.assertEqual(expectedName, actualName, 'I should get white')
+
+
+def suite() -> TestSuite:
+    """You need to change the name of the test class here also."""
+    import unittest
+
+    testSuite: TestSuite = TestSuite()
+    # noinspection PyUnresolvedReferences
+    testSuite.addTest(unittest.makeSuite(TestPyutColorEnum))
+
+    return testSuite
+
+
+if __name__ == '__main__':
+    unitTestMain()
