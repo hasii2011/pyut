790caaa971e95155db94d55b2d9a1fc72f6cb832
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-10-24 22:07:48 -0500

    <Enhancement>[Refactor]: Catch up
    Dependencies need updating
    
    [https://github.com/hasii2011/PyUt/issues/197]

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -13,12 +13,18 @@ if [[ ${STATUS} -eq 0 ]] ; then
     echo "in virtual environment"
     pip3 install --upgrade pip
     pip3 install wheel
+    pip3 install setuptools
     pip3 install wxPython
     pip3 install xmlschema
     pip3 install pygmlparser
+    pip3 install pyumldiagrams
+    pip3 install orthogonal
     pip3 install html-testRunner
     pip3 install py2app
-    pip3 install orthogonal
+    pip3 install antlr4-python3-runtime
+    pip3 install py2app
+    pip3 install networkx
+    pip3 install fpdf2
 else
     echo "You are not in a virtual environment"
 
