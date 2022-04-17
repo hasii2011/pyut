759a7fcbeba8fa8a6d69618dd720cd3e1c35be8a
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2022-04-17 11:26:15 -0500

    <Optimization>[py2app]: <0.28 py2app enables this ⚡️>
    []
    
    [https://github.com/hasii2011/PyUt/issues/327]

--- scripts/packageme.sh
+++ scripts/packageme.sh
@@ -18,14 +18,13 @@ clear
 if [[ $# -eq 0 ]] ; then
         echo "in alias mode"
         rm -rf build dist
-        python3 setup.py py2app -A --iconfile src/org/pyut/resources/img/Pyut.icns
+        python setup.py py2app -A --iconfile src/org/pyut/resources/img/Pyut.icns
 else
     if [[ ${1} = 'deploy' ]] ; then
             echo "in deploy mode"
             rm -rf build dist
-            python3 setup.py py2app --packages=wx,xmlschema,pygmlparser --iconfile src/org/pyut/resources/img/Pyut.icns
+            python -O setup.py py2app --packages=wx,xmlschema,pygmlparser --iconfile src/org/pyut/resources/img/Pyut.icns
     else
         echo "Unknown command line arguments"
     fi
-# rm -rf src/UNKNOWN.egg-info
 fi
