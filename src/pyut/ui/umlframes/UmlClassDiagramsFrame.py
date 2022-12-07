e1e68579d74de972b40bfadd31b47252e1f7c868
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2022-12-06 20:40:17 -0700

    <Refactor>[History Manager]: <Final cleanup>
    []
    
    [https://github.com/hasii2011/PyUt/issues/435]

--- src/pyut/ui/umlframes/UmlClassDiagramsFrame.py
+++ src/pyut/ui/umlframes/UmlClassDiagramsFrame.py
@@ -3,22 +3,12 @@ from typing import cast
 
 from collections import namedtuple
 
-# from pyut.history.commands.CommandGroup import CommandGroup
-# from pyut.history.commands.CreateOglClassCommand import CreateOglClassCommand
-# from pyut.history.commands.CreateOglLinkCommand import CreateOglLinkCommand
-
 from pyutmodel.ModelTypes import ClassName
 from pyutmodel.ModelTypes import Implementors
-from pyutmodel.PyutClass import PyutClass
 from pyutmodel.PyutInterface import PyutInterface
-from pyutmodel.PyutLink import PyutLink
 
-from pyutmodel.PyutLinkType import PyutLinkType
-
-from ogl.OglClass import OglClass
-from ogl.OglInterface import OglInterface
 from ogl.OglInterface2 import OglInterface2
-from ogl.OglLinkFactory import getOglLinkFactory
+
 from wx import CommandProcessor
 
 from pyut.ui.umlframes.UmlDiagramsFrame import UmlDiagramsFrame
@@ -57,108 +47,6 @@ class UmlClassDiagramsFrame(UmlDiagramsFrame):
         self.newDiagram()
 
         self.Bind(EVT_CLASS_NAME_CHANGED, self._onClassNameChanged)
-
-    def createClass(self, oglClass: OglClass):
-
-        x, y = oglClass.GetPosition()
-
-        # TODO:  Use new command processor
-        # cmdGroup: CommandGroup         = CommandGroup("Create class")
-        # cmd:     CreateOglClassCommand = CreateOglClassCommand(x=x, y=y, eventEngine=self._eventEngine, oglClass=oglClass)
-        # cmdGroup.addCommand(cmd)
-        # self.getHistory().addCommandGroup(cmdGroup)
-        # cmd.execute()
-
-    def createLink(self, src: OglClass, dst: OglClass, linkType: PyutLinkType = PyutLinkType.AGGREGATION):
-        """
-        Used to create links;  It is still the caller's responsibility to add the created shape to the
-        appropriate diagram
-
-        Args:
-            src:        The source OglClass
-            dst:        The destination OglClass
-            linkType:   The type of link
-        """
-        sourceClass:      PyutClass = cast(PyutClass, src.pyutObject)
-        destinationClass: PyutClass = cast(PyutClass, dst.pyutObject)
-
-        pyutLink: PyutLink = PyutLink("", linkType=linkType, source=sourceClass, destination=destinationClass)
-
-        oglLinkFactory = getOglLinkFactory()
-        oglLink = oglLinkFactory.getOglLink(src, pyutLink, dst, linkType)
-
-        src.addLink(oglLink)
-        dst.addLink(oglLink)
-
-        pyutClass: PyutClass = cast(PyutClass, src.pyutObject)
-        pyutClass.addLink(pyutLink)
-
-        return oglLink
-
-    def createInheritanceLink(self, child: OglClass, parent: OglClass):
-        """
-        Add a parent link between the child and parent objects.
-
-        Args:
-            child:  Child PyutClass
-            parent: Parent PyutClass
-
-        """
-        # cmdGroup: CommandGroup         = CommandGroup('Creating an inheritance link')
-        # cmd:      CreateOglLinkCommand = CreateOglLinkCommand(src=child, dst=parent)    # inheritance points back to parent
-        # cmdGroup.addCommand(cmd)
-        # self._historyManager.addCommandGroup(cmdGroup)
-        #
-        # cmd.execute()
-        pass    # TODO use new CommandProcessor
-
-    def createInterfaceLink(self, src: OglClass, dst: OglClass) -> OglInterface:
-        """
-        Adds an OglInterface link between src and dst.
-
-        Args:
-            src:    source of the link
-            dst:    destination of the link
-
-        Returns: the created OglInterface link
-        """
-        sourceClass:      PyutClass = cast(PyutClass, src.pyutObject)
-        destinationClass: PyutClass = cast(PyutClass, dst.pyutObject)
-
-        pyutLink:     PyutLink     = PyutLink(linkType=PyutLinkType.INTERFACE, source=sourceClass, destination=destinationClass)
-        oglInterface: OglInterface = OglInterface(srcShape=src, pyutLink=pyutLink, dstShape=dst)
-
-        src.addLink(oglInterface)
-        dst.addLink(oglInterface)
-
-        self._diagram.AddShape(oglInterface)
-        self.Refresh()
-
-        return oglInterface
-
-    def createClasses(self, name: str, x: int, y: int) -> CreatedClassesType:
-        """
-        Create a pair of classes (pyutClass and oglClass)
-
-        Args:
-            name: Class Name
-
-            x:  x-coordinate on the uml frame  oglClass
-            y:  y coordinate on the uml frame  oglClass
-
-        Returns: A named tuple with attributes:  pyutClass and oglClass
-        """
-        pyutClass: PyutClass = PyutClass()
-        pyutClass.name = name
-
-        oglClass: OglClass = OglClass(pyutClass, 50, 50)
-        # To make this code capable of being debugged
-        oglClass.SetPosition(x=x, y=y)
-        self.addShape(oglClass, x, y)
-
-        createdClasses: CreatedClassesType = CreatedClassesType(pyutClass=pyutClass, oglClass=oglClass)
-
-        return createdClasses
 
     def _onClassNameChanged(self, event: ClassNameChangedEvent):
 
